bdcf1efb59d0d7829365535065f5ad5bc9b59a36
Gabriel Florit <gabrielflorit@gmail.com>
2015-09-11 15:56:09 -0400

    support new del api

--- templates/gulp-tasks/clean.js
+++ templates/gulp-tasks/clean.js
@@ -3,10 +3,14 @@ var del = require('del');
 
 //clear all dev folders and sass cache
 gulp.task('clean-dev', function(cb) {
-	del(['.sass-cache'], cb);
+	del(['.sass-cache']).then(function() {
+		cb();
+	});
 });
 
 //clear all prod folders and tmp dir
 gulp.task('clean-prod', function(cb) {
-	del(['.tmp/**', 'dist/prod/**'], cb);
+	del(['.tmp/**', 'dist/prod/**']).then(function() {
+		cb();
+	});
 });
