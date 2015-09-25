9ad2ea0ffad922f0fce6c7e395491825c0f37c71
russell goldenberg <russellgoldenberg@gmail.com>
2015-09-25 13:17:06 -0400

    glob pattern update stylus

--- templates/gulp-tasks/default.js
+++ templates/gulp-tasks/default.js
@@ -3,7 +3,7 @@ var runSequence = require('run-sequence');
 
 // Default task to be run with `gulp`
 gulp.task('default', ['dev'], function() {
-	gulp.watch('src/css/*.styl', ['css-dev']);
+	gulp.watch('src/css/**/*.styl', ['css-dev']);
 	gulp.watch('src/data/*.json', ['html-dev']);
 	gulp.watch('src/html/**/*.hbs', ['html-dev']);
 	gulp.watch('src/js/**/*.js', ['js-dev']);
