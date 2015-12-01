1ba37b67a103c74d7b678d24efc5da84c274e399
russellgoldenberg <russellgoldenberg@gmail.com>
2015-12-01 15:41:21 -0500

    fixed upload

--- templates/gulp-tasks/ssh.js
+++ templates/gulp-tasks/ssh.js
@@ -4,16 +4,17 @@ var shell = require('shelljs');
 var configPath = process.cwd() + '/config.js';
 var config = require(configPath).deploy.ssh;
 
-var command = '(cd dist/prod; scp -rpC . ' + config.username + '@' + config.host + ':' + config.filepath + ')';
+var command = '(cd dist/prod; scp -r . ' + config.username + '@' + config.host + ':' + config.filepath + ')';
 
 gulp.task('ssh-prod', function(cb) {
 	if (configured()) {
-		shell.exec(command);
+		shell.exec(command, function(code, output) {
+			cb();
+		});
 	} else {
 		console.log('*** setup ssh-config.js to automatically upload to apps ***');
+		cb();
 	}
-
-	cb();
 });
 
 function configured() {
