9604a3b3ca4d0d9cfe4a491e55c8d7b941a0660b
Gabriel Florit <gabrielflorit@gmail.com>
2015-12-18 18:29:52 -0500

    Use webpack to minify js

--- templates/gulp-tasks/js-webpack.js
+++ templates/gulp-tasks/js-webpack.js
@@ -2,6 +2,7 @@ const gulp        = require('gulp');
 const rename      = require('gulp-rename');
 const browserSync = require('browser-sync');
 const webpack     = require('webpack-stream');
+const wb          = require('./../node_modules/webpack');
 
 const config = {
 	module: {
@@ -22,8 +23,19 @@ gulp.task('js-dev', function() {
 });
 
 gulp.task('js-prod', function() {
+
+	const prod_config = Object.assign({}, config, {
+
+		plugins: [
+			new wb.optimize.UglifyJsPlugin(),
+			new wb.optimize.OccurenceOrderPlugin(),
+			new wb.optimize.DedupePlugin()
+		]
+
+	});
+
 	return gulp.src('src/js/main.js')
-		.pipe(webpack(config))
+		.pipe(webpack(prod_config))
 		.pipe(rename('bundle.js'))
 		.pipe(gulp.dest('.tmp/js'))
 });
