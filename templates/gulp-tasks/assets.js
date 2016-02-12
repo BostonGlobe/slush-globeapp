529fc209b5c3dc94d6c1773689e2485648c5bc87
russellgoldenberg <russellgoldenberg@gmail.com>
2016-02-12 08:13:01 -0500

    fix #49 reload on new assets

--- templates/gulp-tasks/assets.js
+++ templates/gulp-tasks/assets.js
@@ -1,11 +1,13 @@
-const gulp    = require('gulp');
-const changed = require('gulp-changed');
-const src     = 'src/assets/**/*';
+const gulp 			= require('gulp');
+const changed 		= require('gulp-changed');
+const browserSync   = require('browser-sync');
+const src     		= 'src/assets/**/*';
 
 gulp.task('assets-dev', function() {
 	return gulp.src(src)
 		.pipe(changed('dist/dev/assets'))
-		.pipe(gulp.dest('dist/dev/assets'));
+		.pipe(gulp.dest('dist/dev/assets'))
+		.pipe(browserSync.reload({stream:true}));
 });
 
 // move assets files to prod folder
