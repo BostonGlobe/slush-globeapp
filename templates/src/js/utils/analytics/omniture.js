4af580b991d3e62ba6232cf4ae07e4ce31164ace
russellgoldenberg <russellgoldenberg@gmail.com>
2016-02-19 17:57:42 -0500

    omni update

--- templates/src/js/utils/analytics/omniture.js
+++ templates/src/js/utils/analytics/omniture.js
@@ -2,22 +2,25 @@ import loadJS from '../misc/loadJS.js';
 import meta from '../../../../data/meta';
 
 const omniture = {
-	// loadJS is in base-critical.hbs
 	load: function(cb) {
 		loadJS('https://apps.bostonglobe.com/common/js/omniture/s_code_bgcom.27.5.js', cb);
 	},
 
 	setupTracking: function(showPaywall) {
-		s.pageName= meta.section | meta.title;
-		s.channel= meta.section;
-		s.prop1= meta.section | 'Specials';
-		s.prop6= 'Infographic';
-		s.prop41=s.eVar41='BostonGlobe.com';
+		s.pageName = `${meta.section} | ${meta.title}`;
+		s.channel = meta.section;
+		s.prop1 = `${meta.section} | Specials`;
+		s.prop6 = 'Infographic';
+		s.prop41 = s.eVar41='BostonGlobe.com';
 		s.eVar20 = methode.subscribed ? 'logged in' : 'logged out';
 		s.prop35 = methode.subscribed ? 'logged in' : 'logged out';
 		s.prop3 = meta.author;
 		s.prop67 = meta.page_id;
 		s.eVar67 = meta.page_id;
+
+		if(methode && methode.freeviewCountIncremented) {
+			s.prop48 = methode.freeviewCount;
+		}
 
 		// PAYWALL
 		if (showPaywall) {
