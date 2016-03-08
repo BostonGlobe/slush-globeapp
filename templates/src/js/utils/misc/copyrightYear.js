97f279c213e1886a9af23818eddb027f312ff7db
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-08 08:39:47 -0500

    remove copyright year code

--- templates/src/js/utils/misc/copyrightYear.js
+++
@@ -1,5 +0,0 @@
-export default function copyrightYear() {
-	const year = new Date().getFullYear()
-	const el = document.querySelector('.g-footer--copyright-year')
-	if (el.length) el[0].innerHTML = year
-}
--- templates/src/js/utils/setupDefaultApps.js
+++ templates/src/js/utils/setupDefaultApps.js
@@ -1,7 +1,6 @@
 import setPathCookie from './misc/setPathCookie'
 import wireSocialButtons from './misc/wireSocialButtons'
 import removeMobileHover from './misc/removeMobileHover'
-import copyrightYear from './misc/copyrightYear'
 
 // some default functionality needed to setup apps
 export default function setupDefaultApps() {
