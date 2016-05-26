9d4fc56c5663f2ee1f2a7daae9504c62e16b7f4a
russellgoldenberg <russellgoldenberg@gmail.com>
2016-05-26 10:54:46 -0400

    remove social wiring

--- templates/src/js/utils/setupDefaultApps.js
+++ templates/src/js/utils/setupDefaultApps.js
@@ -1,6 +1,5 @@
 import loadFont from './misc/loadFont'
 import setPathCookie from './misc/setPathCookie'
-import wireSocialButtons from './misc/wireSocialButtons'
 import removeMobileHover from './misc/removeMobileHover'
 
 // some default functionality needed to setup apps
@@ -9,7 +8,6 @@ export default function setupDefaultApps() {
 		{ family: 'Miller', suffix: 'regular', weight: 400 },
 		{ family: 'Benton', suffix: 'bold', weight: 600 },
 	])
-	wireSocialButtons({ facebook: '.share-fb', twitter: '.share-tw' })
 	removeMobileHover()
 	setPathCookie()
 }
