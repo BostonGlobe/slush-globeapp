c2510dbbaf4f1a3cb70dbbbd7a02e8195751a6aa
russellgoldenberg <russellgoldenberg@gmail.com>
2016-07-05 10:45:04 -0400

    fix #133 tracker snippet

---
+++ templates/src/js/utils/track.js
@@ -0,0 +1,11 @@
+const track = (value) => {
+	if (window.location.hostname.indexOf('localhost') > -1) console.log(val)
+	if (typeof s_gi === 'function') {
+		const tracker = s_gi('nytbostonglobecom')
+		s.linkTrackVars = 'eVar15,prop43'
+		s.linkTrackEvents = 'none'
+		tracker.tl(true, 'o', value)
+	}	
+}
+
+export default track
