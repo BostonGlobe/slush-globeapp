933244316e0a8398653e5bca31a12398168f7812
Kevin Wall <kowall116@gmail.com>
2016-11-04 13:42:26 -0700

    Added optional social button to header

--- templates/src/js/app.js
+++ templates/src/js/app.js
@@ -1,8 +1,17 @@
 import setPathCookie from './utils/setPathCookie'
 import removeMobileHover from './utils/removeMobileHover'
+import wireSocialButtons from './utils/wireSocialButtons'
 
 removeMobileHover()
 setPathCookie()
 
 // Add class to html if JS is loaded
 document.getElementsByTagName('html')[0].className+='js-loaded'
+
+// Wire header social if present
+if(document.querySelectorAll('.g-header__share').length > 0) {
+	wireSocialButtons({
+		facebook: '.g-header__share-button--fb',
+		twitter: '.g-header__share-button--tw'
+	})
+}
