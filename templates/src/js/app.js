19f9ec9f7bdc93c752fe225523ada20e2e7d50a9
Gabriel Florit <gabrielflorit@gmail.com>
2016-11-28 15:47:54 -0500

    Update app.js

--- templates/src/js/app.js
+++ templates/src/js/app.js
@@ -6,12 +6,12 @@ removeMobileHover()
 setPathCookie()
 
 // Add class to html if JS is loaded
-document.getElementsByTagName('html')[0].className+='js-loaded'
+document.querySelector('html').classList.add('has-loaded')
 
 // Wire header social if present
-if(document.querySelectorAll('.g-header__share').length > 0) {
+if (document.querySelectorAll('.g-header__share').length > 0) {
 	wireSocialButtons({
 		facebook: '.g-header__share-button--fb',
-		twitter: '.g-header__share-button--tw'
+		twitter: '.g-header__share-button--tw',
 	})
 }
