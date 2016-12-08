be84bdea5934468380b84140aef8b414dfb4fac9
elaina <elaina.natario@gmail.com>
2016-12-08 14:14:06 -0500

    more better report error

--- templates/report-error.js
+++ templates/report-error.js
@@ -1,30 +1,34 @@
 'use strict';
 
-const notify = require('gulp-notify');
-const gutil  = require('gulp-util');
+const notify = require('gulp-notify')
+const gutil  = require('gulp-util')
+const shell = require('shelljs')
 
 module.exports = function(error) {
-	const lineNumber = (error.lineNumber) ? 'LINE ' + error.lineNumber + ' -- ' : '';
-
+	const lineNumber = (error.lineNumber) ? 'LINE ' + error.lineNumber + ' -- ' : ''
+	const name = shell.exec('whoami', {silent:true}).stdout
+	
 	notify({
 		title: 'Task failed [' + error.plugin + ']',
+		subtitle: 'Goddamnit, ' + name,
 		message: lineNumber + 'See terminal.',
+		icon: 'https://apps.bostonglobe.com/common/img/disapproving.png',
 		sound: 'Sosumi'
-	}).write(error);
+	}).write(error)
 
-	gutil.beep();
+	gutil.beep()
 
-	let report = '';
-	let chalk = gutil.colors.white.bgRed;
+	let report = ''
+	let chalk = gutil.colors.white.bgRed
 
-	report += chalk('TASK:') + ' [' + error.plugin + ']\n';
-	report += chalk('PROB:') + ' ' + error.message + '\n';
+	report += chalk('TASK:') + ' [' + error.plugin + ']\n'
+	report += chalk('PROB:') + ' ' + error.message + '\n'
 
-	if (error.lineNumber) { report += chalk('LINE:') + ' ' + error.lineNumber + '\n'; }
+	if (error.lineNumber) { report += chalk('LINE:') + ' ' + error.lineNumber + '\n' }
 
-	if (error.fileName)   { report += chalk('FILE:') + ' ' + error.fileName + '\n'; }
+	if (error.fileName)   { report += chalk('FILE:') + ' ' + error.fileName + '\n' }
 
-	console.error(report);
+	console.error(report)
 
-	this.emit('end');
-};
+	this.emit('end')
+}
