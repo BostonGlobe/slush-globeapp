3e2c2f8eb400c4a38db89e74125b107a34f73b51
datar <datar@mba-mnr-datar.globe.nemgint.com>
2018-02-16 15:50:46 -0500

    removed console log, minor cleanup

--- slushfile.js
+++ slushfile.js
@@ -1,6 +1,6 @@
 const yarn        = require('gulp-yarn')
 const gulp        = require('gulp')
-const template		= require('gulp-template')
+const template    = require('gulp-template')
 const inquirer    = require('inquirer')
 const runSequence = require('run-sequence')
 const shell       = require('shelljs')
@@ -9,124 +9,122 @@ const pkg         = require('./package.json')
 
 // http://stackoverflow.com/a/196991/64372
 function toTitleCase(str) {
-	return str.replace(/\w\S*/g, function(txt) {
-		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
-	})
+  return str.replace(/\w\S*/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+  })
 }
 
 function getGraphicName() {
-	return s.slugify(shell.pwd().split('/').slice(-1)[0])
+  return s.slugify(shell.pwd().split('/').slice(-1)[0])
 }
 
 gulp.task('copy', function(done) {
-	const log = (err) => {
-		console.log(err);
-	}
-	inquirer.prompt([
-		{
-			type: 'list',
-			name: 'projectType',
-			message: 'Select a project type',
-			choices: [
-				'Default',
-				'Multipage'
-			],
-			default: 'Default'
-		}
-	]).then(function(answers) {
-		const srcdirs = answers.projectType === 'Multipage' ? [
-			__dirname + '/templates/default/**',
-			__dirname + '/templates/multipage/**'
-		] : __dirname + '/templates/default/**'
-		return gulp.src(srcdirs, {dot: true})
-			.pipe(template(answers, { interpolate: /${{{([\s\S]+?)}}}$/g }))
-			.pipe(gulp.dest('./'))
-			.on('end', done)
-	}).catch(err => { console.log(err); })
+  const log = (err) => {
+    console.log(err);
+  }
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'projectType',
+      message: 'Select a project type',
+      choices: [
+        'Default',
+        'Multipage'
+      ],
+      default: 'Default'
+    }
+  ]).then(function(answers) {
+    const srcdirs = answers.projectType === 'Multipage' ? [
+      __dirname + '/templates/default/**',
+      __dirname + '/templates/multipage/**'
+    ] : __dirname + '/templates/default/**'
+    return gulp.src(srcdirs, {dot: true})
+      .pipe(template(answers, { interpolate: /${{{([\s\S]+?)}}}$/g }))
+      .pipe(gulp.dest('./'))
+      .on('end', done)
+  }).catch(err => { console.log(err); })
 })
 
 gulp.task('install', function() {
-	return gulp.src('./package.json')
-		.pipe(yarn())
+  return gulp.src('./package.json')
+    .pipe(yarn())
 })
 
 gulp.task('setup-ssh', function(done) {
-	inquirer.prompt([
-		{
-			type: 'list',
-			name: 'section',
-			message: 'Select a section',
-			choices: [
-				'arts',
-				'business',
-				'lifestyle',
-				'magazine',
-				'metro',
-				'news/nation',
-				'news/politics',
-				'news/world',
-				'opinion',
-				'sports',
-			]
-		}
-	]).then(function(answers) {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'section',
+      message: 'Select a section',
+      choices: [
+        'arts',
+        'business',
+        'lifestyle',
+        'magazine',
+        'metro',
+        'news/nation',
+        'news/politics',
+        'news/world',
+        'opinion',
+        'sports',
+      ]
+    }
+  ]).then(function(answers) {
 
-		const now = new Date()
-		const year = now.getFullYear()
-		const month = ((now.getMonth() + 1) < 10 ? '0' : '') + (now.getMonth() + 1)
-		const section = answers.section
-		const sectionTitled = toTitleCase(section.split('/').slice(-1)[0])
-		const url = `${section}/graphics/${year}/${month}/${getGraphicName()}`
-		const sectionUrl = `http://www.bostonglobe.com/${section}`
+    const now = new Date()
+    const year = now.getFullYear()
+    const month = ((now.getMonth() + 1) < 10 ? '0' : '') + (now.getMonth() + 1)
+    const section = answers.section
+    const sectionTitled = toTitleCase(section.split('/').slice(-1)[0])
+    const url = `${section}/graphics/${year}/${month}/${getGraphicName()}`
+    const sectionUrl = `http://www.bostonglobe.com/${section}`
 
-		console.log('Setting app url to /' + url)
+    console.log('Setting app url to /' + url)
 
-		// add correct url to config.json
-		shell.sed('-i', 'PATH-TO-APP', url, 'data/config.json')
+    // add correct url to config.json
+    shell.sed('-i', 'PATH-TO-APP', url, 'data/config.json')
 
-		// add correct path to meta.json
-		shell.sed('-i', 'path', `https://apps.bostonglobe.com/${url}`, 'data/meta.json')
+    // add correct path to meta.json
+    shell.sed('-i', 'path', `https://apps.bostonglobe.com/${url}`, 'data/meta.json')
 
-		// add correct section to meta.json
-		shell.sed('-i', 'section', sectionTitled, 'data/meta.json')
+    // add correct section to meta.json
+    shell.sed('-i', 'section', sectionTitled, 'data/meta.json')
 
-		// add correct sectionUrl to meta.json
-		shell.sed('-i', 'sectionUrl', sectionUrl, 'data/meta.json')
+    // add correct sectionUrl to meta.json
+    shell.sed('-i', 'sectionUrl', sectionUrl, 'data/meta.json')
 
-		// add correct year to LICENSE
-		shell.sed('-i', 'YEAR', year, 'LICENSE')
+    // add correct year to LICENSE
+    shell.sed('-i', 'YEAR', year, 'LICENSE')
 
-		// add correct graphic name to README
-		shell.sed('-i', /APPNAME/g, getGraphicName(), 'README.md')
+    // add correct graphic name to README
+    shell.sed('-i', /APPNAME/g, getGraphicName(), 'README.md')
 
-		done()
+    done()
 
-	})
+  })
 
 })
 
 gulp.task('check-for-updates', function(done) {
-	console.log(shell.exec('npm view slush-globeapp version', {silent:true}))
+  const latestVersion = shell.exec('npm view slush-globeapp version', {silent:true}).stdout.split('\n')[0]
+  const installedVersion = pkg.version
 
-	const latestVersion = shell.exec('npm view slush-globeapp version', {silent:true}).stdout.split('\n')[0]
-	const installedVersion = pkg.version
-
-	if (latestVersion !== installedVersion) {
-		console.log('Your version of slush-globeapp is outdated. Please update and try again.')
-		shell.exit(1)
-	} else {
-		console.log('Using version ' + installedVersion)
-		done()
-	}
+  if (latestVersion !== installedVersion) {
+    console.log('Your version of slush-globeapp is outdated. Please update and try again.')
+    shell.exit(1)
+  } else {
+    console.log('Using version ' + installedVersion)
+    done()
+  }
 
 })
 
 gulp.task('default', function(done) {
-	runSequence(
-		'check-for-updates',
-		'copy',
-		'install',
-		'setup-ssh',
-		done
-	)
+  runSequence(
+    'check-for-updates',
+    'copy',
+    'install',
+    'setup-ssh',
+    done
+  )
 })
