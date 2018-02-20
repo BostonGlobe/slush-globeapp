75d84c769cb3f485c9080b003431a4c56ad00841
datar <datar@mba-mnr-datar.globe.nemgint.com>
2018-02-20 11:29:02 -0500

    got local prod server working on default template

--- templates/default/gulp-tasks/browser-sync.js
+++ templates/default/gulp-tasks/browser-sync.js
@@ -1,12 +1,14 @@
 const gulp = require('gulp')
 const browserSync = require('browser-sync')
+const args = require('yargs').argv
+const baseDir = args.prod ? 'dist/prod/' : 'dist/dev/'
 
 // browser-sync task for starting the server.
 gulp.task('browser-sync', () => {
 	browserSync({<% if(projectType === 'Multipage') { %>
 		proxy: 'http://localhost:5000',<% } else { %>
 		server: {
-			baseDir: 'dist/dev/',
+			baseDir: baseDir,
 			index: 'index.html'
 		},<% } %>
 		notify: false,
