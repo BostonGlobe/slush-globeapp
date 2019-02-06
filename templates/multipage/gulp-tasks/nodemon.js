1c20880aebd82094d19111d2edbce695c75a02e8
datar <datar@mba-mnr-datar2.globe.nemgint.com>
2019-02-06 16:53:16 -0500

    spacing changes to nodemon.js in multipage

--- templates/multipage/gulp-tasks/nodemon.js
+++ templates/multipage/gulp-tasks/nodemon.js
@@ -3,16 +3,16 @@ var nodemon = require('gulp-nodemon');
 
 gulp.task('nodemon', function (cb) {
 
-	var started = false;
+  var started = false;
 
-	return nodemon({
-		script: 'server.js'
-	}).on('start', function () {
-		// to avoid nodemon being started multiple times
-		// thanks @matthisk
-		if (!started) {
-			cb();
-			started = true;
-		}
-	});
+  return nodemon({
+    script: 'server.js'
+  }).on('start', function () {
+    // to avoid nodemon being started multiple times
+    // thanks @matthisk
+    if (!started) {
+      cb();
+      started = true;
+    }
+  });
 });
